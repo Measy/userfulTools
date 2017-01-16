cf3374ebfadef1b55a0267c9cb62070ac30ca6ef
Measy <1608500736@qq.com>
2017-01-16 18:49:39 +0800

    flux prictice

---
+++ react-flux/dispatcher/AppDispatcher.js
@@ -0,0 +1,17 @@
+var Dispatcher = require('flux').Dispatcher;
+var AppDispatcher = new Dispatcher();
+var ListStore = require('../stores/ListStore');
+
+AppDispatcher.register(function (action) {
+    switch (action.actionType) {
+        case "ADD_NEW_ITEM":
+            ListStore.addNewItemHandler(action.text);
+            ListStore.emitChange();
+            break;
+    
+        default:
+            break;
+    }
+})
+
+module.exports = AppDispatcher;
