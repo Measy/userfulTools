cb31dcfa40bdcbc1ab7b3bedba04cdc3ab2e7e5c
1608500736@qq.com <1608500736@qq.com>
2017-01-22 12:18:32 +0800

    add react redux demo counter

---
+++ redux-demo/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import Counter from './components/Counter'
+import counter from './reducers'
+
+const store = createStore(counter);
+const rootEle = document.getElementById('root');
+
+const render = () => ReactDOM.render(
+    <Counter
+        value={store.getState()}
+        onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+        onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+        />,
+    rootEle
+)
+
+render()
+store.subscribe(render)
