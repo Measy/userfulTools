91d5124cc55dc9c152c75f66b79b60bca1d285ac
Measy <1608500736@qq.com>
2017-02-09 00:05:33 +0800

    增加留言板RESTful API后台模块

---
+++ msg-board-server/middlewares/simpleLogger.js
@@ -0,0 +1,4 @@
+module.exports = function (req, res, next) {
+    console.info('[LOG]', req.method, req.originalUrl);
+    next();
+}
---
+++ msg-board-server/middlewares/simpleUserSession.js
@@ -0,0 +1,8 @@
+var userService = require('../services/userService');
+
+module.exports = function (req, res, next) {
+    if(!req.session) req.session = {};
+    req.session.userData = userService.isLogin();
+
+    next();
+};
