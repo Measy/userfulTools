ff15ff60f25773e6c2f27008bbce30b22e6a1888
1608500736@qq.com <1608500736@qq.com>
2017-03-02 21:31:37 +0800

    完美运行

--- react-msg-board/build/webpack.dev.conf.js
+++ react-msg-board/build/webpack.dev.conf.js
@@ -1,61 +1,57 @@
 var webpack = require('webpack'),
-    config = require('./webpack.base.conf'),
-    HtmlWebpackPlugin = require('html-webpack-plugin'),
-    ExtractTextPlugin = require('extract-text-webpack-plugin'),
-    BrowserSyncPlugin = require('browser-sync-webpack-plugin'),
-    SOURCE_MAP = false;
+  config = require('./webpack.base.conf'),
+  HtmlWebpackPlugin = require('html-webpack-plugin'),
+  ExtractTextPlugin = require('extract-text-webpack-plugin'),
+  BrowserSyncPlugin = require('browser-sync-webpack-plugin'),
+  // SOURCE_MAP = true; // 大多数情况下用不到
+  SOURCE_MAP = false;
 
 config.output.filename = '[name].js';
 config.output.chunkFilename = '[id].js';
 
 config.devtool = SOURCE_MAP ? 'eval-source-map' : false;
 
-//add hot-reload related conde to entry chunk
+// add hot-reload related code to entry chunk
 config.entry.app = [
-    'eventsource-polyfill',
-    'webpack-hot-middleware/client?reload=true',
-    'webpack/hot/only-dev-server',
-    config.entry.app
+  'eventsource-polyfill',
+  'webpack-hot-middleware/client?reload=true',
+  'webpack/hot/only-dev-server',
+  config.entry.app
 ];
 
 config.output.publicPath = '/';
 
-//开发环境下直接内嵌CSS以支持热替换
-config.module.loaders.push(
-    {
-        test: /\.css$/,
-        loader: 'style!css'
-    }, {
-        test: /\.less$/,
-        loader: 'style!css!less'
-    }, {
-        test: /\.scss$/,
-        loader: 'style!css!sass'
-    }
-);
-
+// 开发环境下直接内嵌 CSS 以支持热替换
+config.module.loaders.push({
+  test: /\.css$/,
+  loader: 'style!css'
+}, {
+  test: /\.less$/,
+  loader: 'style!css!less'
+}, {
+  test: /\.scss$/,
+  loader: 'style!css!sass'
+});
 
 config.plugins.push(
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(), //允许错误不打断程序运行
-    new ExtractTextPlugin('[name].css'),
-    new HtmlWebpackPlugin({
-        filename: 'index.html',
-        template: config.commonPath.indexHtml,
-        chunksSortMode: 'none' //允许控制块在添加到页面之前的排序方式
-    }),
-    new BrowserSyncPlugin({
-        host: '127.0.0.1',
-        port: 9090,
-        proxy: 'http://127.0.0.1:9090/',
-        logConnections: false,
-        notify: false
-    },{
-        reload: false
-    })
+  new webpack.optimize.OccurenceOrderPlugin(),
+  new webpack.HotModuleReplacementPlugin(),
+  new webpack.NoErrorsPlugin(), // 允许错误不打断程序运行
+  new ExtractTextPlugin('[name].css'),
+  new HtmlWebpackPlugin({
+    filename: 'index.html',
+    template: config.commonPath.indexHTML,
+    chunksSortMode: 'none' // 允许控制块在添加到页面之前的排序方式
+  }),
+  new BrowserSyncPlugin({
+    host: '127.0.0.1',
+    port: 9090,
+    proxy: 'http://127.0.0.1:9000/',
+    logConnections: false,
+    notify: false
+  }, {
+    reload: false
+  })
 );
 
 module.exports = config;
-
-
--- react-msg-board/src/redux/store/middlewares.js
+++ react-msg-board/src/redux/store/middlewares.js
@@ -2,9 +2,9 @@
 // 配置中间件
 // ======================================================
 import thunk from 'redux-thunk'
-import { historyMiddlewre } from './syncHistoryWithStore'
+import { historyMiddleware } from './syncHistoryWithStore'
 
-const middlewares = [thunk, historyMiddlewre]
+const middlewares = [thunk, historyMiddleware]
 
 if (__DEV__) {
     /** Redux Logger (P.S: 打印日志会造成轻微的卡顿) **/
